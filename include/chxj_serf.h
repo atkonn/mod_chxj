5c341f23fa19c0e087f30ee78b4e591dfeb2dd69
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-05-26 12:33:05 +0000

    * temp commit.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2655 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/chxj_serf.h
+++ include/chxj_serf.h
@@ -32,6 +32,7 @@
  */
 #include "serf.h"
 
+
 extern char *default_chxj_serf_get(request_rec *r, apr_pool_t *ppool, const char *url_path);
 extern char * (*chxj_serf_get)(request_rec *r, apr_pool_t *ppool, const char *url_path);
 
