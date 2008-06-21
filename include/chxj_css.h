92c976f1d8ece1e38b32d186b5370020b64cf7d5
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-06-21 07:20:06 +0000

    * temp commit.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2717 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/chxj_css.h
+++ include/chxj_css.h
@@ -43,6 +43,15 @@ typedef struct __css_selector_t {
   css_property_t property_head;
 } css_selector_t;
 
+typedef struct __css_prop_list_t {
+  struct __css_prop_list_t *next;
+  struct __css_prop_list_t **ref;
+  css_property_t head;
+} css_prop_list_t;
+
+typedef struct __css_prop_list_stack_t {
+  css_prop_list_t head;
+} css_prop_list_stack_t;
 
 /**
  * CSS stylesheet.
@@ -52,6 +61,11 @@ typedef struct __css_stylesheet_t {
   css_selector_t selector_head;
 } css_stylesheet_t;
 
+#define STYLESHEETS_SELECTOR_HEAD(X)  (X)->selector_head
+#define STYLESHEETS_FIRST_SELECTOR(X) STYLESHEETS_SELECTOR_HEAD((X))##.next
+#define STYLESHEETS_LAST_SELECTOR(X)  ((apr_size_t)(STYLESHEET_SELECTOR_HEAD((X))##.ref) - (apr_size_t)APR_OFFSETOF(css_selector_t, next))
+#define STYLESHEETS_SENTINEL(X)       &(STYLESHEETS_SELECTOR_HEAD((X)))
+  
 
 /**
  * CSS current_stylesheet.
@@ -78,6 +92,14 @@ extern css_selector_t *chxj_css_find_selector(Doc *doc, css_stylesheet_t *styles
 extern css_stylesheet_t *chxj_find_pseudo_selectors(Doc *doc, css_stylesheet_t *stylesheet);
 extern css_stylesheet_t *chxj_css_parse_style_attr(Doc *doc, css_stylesheet_t *old_stylesheet, char *tag_name, char *class_name, char *id_name, char *style_attr_value);
 extern css_stylesheet_t *chxj_css_parse_style_value(Doc *doc, css_stylesheet_t *old_stylesheet, char *style_attr_value);
+
+extern css_prop_list_stack_t *chxj_new_prop_list_stack(Doc *doc);
+extern css_prop_list_t *chxj_css_create_prop_list(Doc *doc, css_selector_t *sel);
+extern void chxj_css_push_prop_list(css_prop_list_stack_t *stack, css_prop_list_t *prop_list);
+extern css_prop_list_t *chxj_css_pop_prop_list(css_prop_list_stack_t *stack);
+extern css_prop_list_t *chxj_dup_css_prop_list(Doc *doc, css_prop_list_t *old);
+extern void chxj_css_prop_list_merge_property(Doc *doc, css_prop_list_t *base, css_selector_t *sel);
+extern css_prop_list_t *chxj_css_get_last_prop_list(css_prop_list_stack_t *stack);
 #endif
 /*
  * vim:ts=2 et
