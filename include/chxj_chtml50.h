4b3b0be32a0893c90aaf6396b55d4c595008a2cf
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-06-28 12:35:46 +0000

    * Added LINK tag for CHTML5.0 converter.   * Added test code of the LINK tag for CHTML5.0 converter.   * Added test code of the HTML tag with CSS for CHTML5.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2758 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/chxj_chtml50.h
+++ include/chxj_chtml50.h
@@ -21,6 +21,7 @@
 
 #include "mod_chxj.h"
 #include "chxj_cookie.h"
+#include "chxj_css.h"
 
 /*----------------------------------------------------------------------------*/
 /* Structure for CHTML4.0                                                     */
@@ -28,17 +29,19 @@
 typedef struct chtml50_t chtml50_t;
 
 struct chtml50_t {
-    Doc                 *doc;
-    char                *out;
-    int                 out_len;
-    int                 pre_flag;
-    int                 textarea_flag;
-    int                 font_flag;
+  Doc                 *doc;
+  char                *out;
+  int                 out_len;
+  int                 pre_flag;
+  int                 textarea_flag;
+  int                 font_flag;
 
-    device_table        *spec;
-    mod_chxj_config     *conf;
-    chxjconvrule_entry  *entryp;
-    cookie_t            *cookie;
+  device_table        *spec;
+  mod_chxj_config     *conf;
+  chxjconvrule_entry  *entryp;
+  cookie_t            *cookie;
+  css_stylesheet_t    *style;
+  css_prop_list_stack_t *css_prop_stack;
 };
 
 /*----------------------------------------------------------------------------*/
