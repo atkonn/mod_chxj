afa8f8fa15414da88ab65dc445985cbce2d0e227
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-06-28 18:23:04 +0000

    * Added test code of the META tag with CSS for SoftBank XHTML converter.   * Added test code of the META tag with CSS for au XHTML converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2768 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
+++ test/chxj_xhtml_mobile_1_0/test_chxj_xhtml_mobile_1_0.c
@@ -526,6 +526,7 @@ void test_xhtml_link_008();
 void test_xhtml_link_009();
 
 void test_xhtml_html_tag_with_css_001();
+void test_xhtml_meta_tag_with_css_001();
 /* pend */
 
 int
@@ -1089,6 +1090,7 @@ main()
   CU_add_test(xhtml_suite, "test link 009." ,                                    test_xhtml_link_009);
 
   CU_add_test(xhtml_suite, "test html with css 001",                             test_xhtml_html_tag_with_css_001);
+  CU_add_test(xhtml_suite, "test meta with css 001",                             test_xhtml_meta_tag_with_css_001);
   /* aend */
 
   CU_basic_run_tests();
@@ -14168,6 +14170,56 @@ void test_xhtml_html_tag_with_css_001()
   apr_size_t destlen;
   APR_INIT;
   chxj_serf_get = test_chxj_serf_get002;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+  entry.action |= CONVRULE_CSS_ON_BIT;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_xhtml_mobile_1_0(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+
+
+/******************************************************************************/
+/* meta with CSS                                                              */
+/******************************************************************************/
+char *test_chxj_serf_get003(request_rec *r, apr_pool_t *ppool, const char *uri_path, int ss, apr_size_t *len)
+{
+  static char *css = "a:focus { display: none }\n"
+                     "a:link  { display: none }\n"
+                     "a       { display: none }\n"
+                     "hr      { display: none }\n"
+                     "a:visited { display:none }\n"
+                     "meta    { display: none }\n";
+  *len = strlen(css);
+  call_check = 1;
+  return css;
+}
+void test_xhtml_meta_tag_with_css_001()
+{
+#define  TEST_STRING "<html><head><link rel=\"stylesheet\" href=\"http://localhost/a.css\"  type=\"text/css\" />" \
+                     "<META http-equiv=\"Content-Type\" content=\"text/html; charset=SHIFT_JIS\"></head><body></body></html>"
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Windows-31J\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML Basic 1.0//EN\" \"http://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=Windows-31J\" /></head><body></body></html>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get003;
   call_check = 0;
 
   COOKIE_INIT(cookie);
