85dc83fda8571fcddbc9d6e0315c52055a1ad5c4
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-06-28 10:28:22 +0000

    * Added HTML tag with CSS to CHTML1.0 converter.   * Added test code of the HTML tag with CSS for CHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2754 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_chtml10/test_chxj_chtml10.c
+++ test/chxj_chtml10/test_chxj_chtml10.c
@@ -428,6 +428,8 @@ void test_chtml10_link_006();
 void test_chtml10_link_007();
 void test_chtml10_link_008();
 void test_chtml10_link_009();
+
+void test_chtml10_html_tag_with_css_001();
 /* pend */
 
 int
@@ -819,6 +821,8 @@ main()
   CU_add_test(chtml10_suite, "test link 007." ,                                    test_chtml10_link_007); 
   CU_add_test(chtml10_suite, "test link 008." ,                                    test_chtml10_link_008); 
   CU_add_test(chtml10_suite, "test link 009." ,                                    test_chtml10_link_009); 
+
+  CU_add_test(chtml10_suite, "test html with css 001",                             test_chtml10_html_tag_with_css_001);
   /* aend */
 
   CU_basic_run_tests();
@@ -10690,6 +10694,55 @@ void test_chtml10_link_009()
   CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
   CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
   CU_ASSERT(call_check == 0);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+
+
+
+/*===========================================================================*/
+/* HTML tag with CSS                                                         */
+/*===========================================================================*/
+char *test_chxj_serf_get002(request_rec *r, apr_pool_t *ppool, const char *uri_path, int ss, apr_size_t *len)
+{
+  static char *css = "a:focus { display: none }\n"
+                     "a:link  { display: none }\n"
+                     "a       { display: none }\n"
+                     "hr      { display: none }\n"
+                     "a:visited { display:none }\n"
+                     "html    { display: none }\n";
+  *len = strlen(css);
+  call_check = 1;
+  return css;
+}
+void test_chtml10_html_tag_with_css_001()
+{
+#define  TEST_STRING "<html><head><link rel=\"stylesheet\" href=\"http://localhost/a.css\"  type=\"text/css\" /></head><body></body></html>"
+#define  RESULT_STRING "<html><head></head><body></body></html>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get002;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_chtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 1);
 
   APR_TERM;
 #undef TEST_STRING
