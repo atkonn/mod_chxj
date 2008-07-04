059af92f98640e510c94484a5fef3c129dbed2aa
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-04 09:32:39 +0000

    Merge branch 'branch_0.12.0' into sandbox
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2797 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jxhtml.c
+++ src/chxj_jxhtml.c
@@ -728,7 +728,6 @@ s_jxhtml_start_meta_tag(void *pdoc, Node *node)
             sec[url-buf] = 0;
             url++;
             url = chxj_encoding_parameter(r, url);
-            url = chxj_add_cookie_parameter(r, url, jxhtml->cookie);
             W_L(" ");
             W_V(name);
             W_L("=\"");
@@ -1063,7 +1062,6 @@ s_jxhtml_start_a_tag(void *pdoc, Node *node)
       /* CHTML1.0                                                             */
       /*----------------------------------------------------------------------*/
       value = chxj_encoding_parameter(r, value);
-      value = chxj_add_cookie_parameter(r, value, jxhtml->cookie);
       W_L(" href=\"");
       W_V(value);
       W_L("\"");
@@ -2091,7 +2089,6 @@ s_jxhtml_start_img_tag(void *pdoc, Node *node)
       /*----------------------------------------------------------------------*/
 #ifdef IMG_NOT_CONVERT_FILENAME
       value = chxj_encoding_parameter(r, value);
-      value = chxj_add_cookie_parameter(r, value, jxhtml->cookie);
       if (value) {
         value = apr_psprintf(r->pool,
                              "%s%c%s=true",
@@ -2105,7 +2102,6 @@ s_jxhtml_start_img_tag(void *pdoc, Node *node)
 #else
       value = chxj_img_conv(r, spec, value);
       value = chxj_encoding_parameter(r, value);
-      value = chxj_add_cookie_parameter(r, value, jxhtml->cookie);
       if (value) {
         value = apr_psprintf(r->pool,
                              "%s%c%s=true",
