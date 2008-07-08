36d69e8734abbefa95b15bbd33e73fb6e0def282
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-08 11:13:15 +0000

    * Added p tag with CSS for CHTML5.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@2833 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/chxj_chtml50.h
+++ include/chxj_chtml50.h
@@ -44,6 +44,12 @@ struct chtml50_t {
   css_prop_list_stack_t *css_prop_stack;
 };
 
+typedef struct _chtml50_flags_t chtml50_flags_t;
+struct _chtml50_flags_t {
+  int with_font_flag;
+  int with_blink_flag;
+};
+
 /*----------------------------------------------------------------------------*/
 /* Prototype                                                                  */
 /*----------------------------------------------------------------------------*/
