ee2a7e5de56cb40c328100e9561346fbf67cf546
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-22 16:01:39 +0000

    * Added dt tag with CSS for SoftBank XHTML converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3053 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jxhtml.c
+++ src/chxj_jxhtml.c
@@ -3742,11 +3742,64 @@ s_jxhtml_end_dl_tag(void *pdoc, Node *UNUSED(child))
  * @return The conversion result is returned.
  */
 static char *
-s_jxhtml_start_dt_tag(void *pdoc, Node *UNUSED(child))
+s_jxhtml_start_dt_tag(void *pdoc, Node *node)
 {
-  jxhtml_t *jxhtml = GET_JXHTML(pdoc);
-  Doc     *doc   = jxhtml->doc;
-  W_L("<dt>");
+  jxhtml_t  *jxhtml = GET_JXHTML(pdoc);
+  Doc       *doc     = jxhtml->doc;
+  Attr      *attr;
+  char      *attr_style = NULL;
+  char      *attr_font_size = NULL;
+
+  for (attr = qs_get_attr(doc,node);
+       attr;
+       attr = qs_get_next_attr(doc,attr)) {
+    char *nm  = qs_get_attr_name(doc,attr);
+    char *val = qs_get_attr_value(doc,attr);
+    if (val && STRCASEEQ('s','S',"style", nm)) {
+      attr_style = val;
+    }
+  }
+  if (IS_CSS_ON(jxhtml->entryp)) {
+    css_prop_list_t *style = s_jxhtml_push_and_get_now_style(pdoc, node, attr_style);
+    if (style) {
+      css_property_t *font_size_prop    = chxj_css_get_property_value(doc, style, "font-size");
+      css_property_t *cur;
+      for (cur = font_size_prop->next; cur != font_size_prop; cur = cur->next) {
+        if (cur->value && *cur->value) {
+          if (STRCASEEQ('x','X',"xx-small",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "xx-small");
+          }
+          else if (STRCASEEQ('x','X',"x-small",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "x-small");
+          }
+          else if (STRCASEEQ('s','S',"small",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "small");
+          }
+          else if (STRCASEEQ('m','M',"medium",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "medium");
+          }
+          else if (STRCASEEQ('l','L',"large",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "large");
+          }
+          else if (STRCASEEQ('x','X',"x-large",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "x-large");
+          }
+          else if (STRCASEEQ('x','X',"xx-large",cur->value)) {
+            attr_font_size = apr_pstrdup(doc->pool, "xx-large");
+          }
+        }
+      }
+    }
+  }
+  W_L("<dt");
+  if (attr_font_size) {
+    W_L(" style=\"");
+    W_L("font-size:");
+    W_V(attr_font_size);
+    W_L(";");
+    W_L("\"");
+  }
+  W_L(">");
   return jxhtml->out;
 }
 
@@ -3763,6 +3816,11 @@ static char *
 s_jxhtml_end_dt_tag(void *pdoc, Node *UNUSED(child))
 {
   jxhtml_t *jxhtml = GET_JXHTML(pdoc);
+  Doc      *doc    = jxhtml->doc;
+  W_L("</dt>");
+  if (IS_CSS_ON(jxhtml->entryp)) {
+    chxj_css_pop_prop_list(jxhtml->css_prop_stack);
+  }
   return jxhtml->out;
 }
 
