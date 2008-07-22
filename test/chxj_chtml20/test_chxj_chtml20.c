8c6601dd9bcdd2e9012c5557cccbea6f79f873b3
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-22 12:34:39 +0000

    * Added dt tag with CSS for CHTML2.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3047 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_chtml20/test_chxj_chtml20.c
+++ test/chxj_chtml20/test_chxj_chtml20.c
@@ -1631,7 +1631,7 @@ void test_chtml20_a_tag_href_attribute_003()
 void test_chtml20_a_tag_href_attribute_004() 
 {
 #define  TEST_STRING "<html><head></head><body><a href=\"a.html#abc\">abc</a></body></html>"
-#define  RESULT_STRING "<html><head></head><body><a href=\"a.html#abc?_chxj_cc=test_cookie_id\">abc</a></body></html>"
+#define  RESULT_STRING "<html><head></head><body><a href=\"a.html?_chxj_cc=test_cookie_id#abc\">abc</a></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
@@ -1659,7 +1659,7 @@ void test_chtml20_a_tag_href_attribute_004()
 void test_chtml20_a_tag_href_attribute_005() 
 {
 #define  TEST_STRING "<html><head></head><body><a href=\"a.html#\">abc</a></body></html>"
-#define  RESULT_STRING "<html><head></head><body><a href=\"a.html#?_chxj_cc=test_cookie_id\">abc</a></body></html>"
+#define  RESULT_STRING "<html><head></head><body><a href=\"a.html?_chxj_cc=test_cookie_id#\">abc</a></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
