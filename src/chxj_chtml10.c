10fdc358fae7c613f4fd22e9f7b46c9e1af60fb8
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-25 14:30:25 +0000

    * Added dir tag with CSS for CHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3078 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -3509,13 +3509,30 @@ s_chtml10_end_blockquote_tag(void *pdoc, Node *UNUSED(child))
  * @return The conversion result is returned.
  */
 static char *
-s_chtml10_start_dir_tag(void *pdoc, Node *UNUSED(child))
+s_chtml10_start_dir_tag(void *pdoc, Node *node)
 {
   chtml10_t *chtml10;
-  Doc *doc;
+  Doc       *doc;
+  Attr      *attr;
+  char      *attr_style = NULL;
+
   chtml10 = GET_CHTML10(pdoc);
   doc     = chtml10->doc;
+
+  for (attr = qs_get_attr(doc,node);
+       attr;
+       attr = qs_get_next_attr(doc,attr)) {
+    char *nm  = qs_get_attr_name(doc,attr);
+    char *val = qs_get_attr_value(doc,attr);
+    if (val && STRCASEEQ('s','S',"style", nm)) {
+      attr_style = val;
+    }
+  }
+
   W_L("<dir>");
+  if (IS_CSS_ON(chtml10->entryp)) {
+    s_chtml10_push_and_get_now_style(pdoc, node, attr_style);
+  }
   return chtml10->out;
 }
 
@@ -3536,7 +3553,11 @@ s_chtml10_end_dir_tag(void *pdoc, Node *UNUSED(child))
 
   chtml10 = GET_CHTML10(pdoc);
   doc     = chtml10->doc;
+
   W_L("</dir>");
+  if (IS_CSS_ON(chtml10->entryp)) {
+    chxj_css_pop_prop_list(chtml10->css_prop_stack);
+  }
   return chtml10->out;
 }
 
