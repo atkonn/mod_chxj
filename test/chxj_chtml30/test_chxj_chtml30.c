c7363f2824f881559860a0a004f831010d19939c
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-25 14:30:33 +0000

    * Added dir tag with CSS for CHTML3.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3080 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_chtml30/test_chxj_chtml30.c
+++ test/chxj_chtml30/test_chxj_chtml30.c
@@ -663,6 +663,9 @@ void test_chtml30_div_tag_with_css_028();
 
 void test_chtml30_blockquote_tag_with_css_001();
 void test_chtml30_blockquote_tag_with_css_002();
+
+void test_chtml30_dir_tag_with_css_001();
+void test_chtml30_dir_tag_with_css_002();
 /* pend */
 
 int
@@ -1287,6 +1290,9 @@ main()
 
   CU_add_test(chtml30_suite, "test blockquote with css 001",                       test_chtml30_blockquote_tag_with_css_001);
   CU_add_test(chtml30_suite, "test blockquote with css 002",                       test_chtml30_blockquote_tag_with_css_002);
+
+  CU_add_test(chtml30_suite, "test dir with css 001",                       test_chtml30_dir_tag_with_css_001);
+  CU_add_test(chtml30_suite, "test dir with css 002",                       test_chtml30_dir_tag_with_css_002);
   /* aend */
 
   CU_basic_run_tests();
@@ -19031,6 +19037,95 @@ void test_chtml30_blockquote_tag_with_css_002()
   apr_size_t destlen;
   APR_INIT;
   chxj_serf_get = test_chxj_serf_get150;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+  entry.action |= CONVRULE_CSS_ON_BIT;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_chtml30(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 0);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+
+
+
+
+
+
+
+/*===========================================================================*/
+/* dir tag with CSS                                                          */
+/*===========================================================================*/
+char *test_chxj_serf_get180(request_rec *r, apr_pool_t *ppool, const char *uri_path, int ss, apr_size_t *len)
+{
+  static char *css = "a:focus { display: none }\n"
+                     "a:link  { display: none }\n"
+                     "a       { display: none }\n"
+                     "hr      { display: none }\n"
+                     "a:visited { display:none }\n"
+                     "dir { color: #ff0000 }\n";
+
+  *len = strlen(css);
+  call_check = 1;
+  return css;
+}
+void test_chtml30_dir_tag_with_css_001()
+{
+#define  TEST_STRING "<html><head><link rel=\"stylesheet\" href=\"http://localhost/a.css\"  type=\"text/css\" />" \
+                     "</head><body><dir>あいう</dir></body></html>"
+#define  RESULT_STRING "<html><head></head><body><dir><font color=\"#ff0000\">あいう</font></dir></body></html>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get180;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+  entry.action |= CONVRULE_CSS_ON_BIT;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_chtml30(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 1);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_chtml30_dir_tag_with_css_002()
+{
+#define  TEST_STRING "<html><head>" \
+                     "</head><body><dir style=\"color:#ff0000\">あいう</dir></body></html>"
+#define  RESULT_STRING "<html><head></head><body><dir><font color=\"#ff0000\">あいう</font></dir></body></html>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get180;
   call_check = 0;
 
   COOKIE_INIT(cookie);
