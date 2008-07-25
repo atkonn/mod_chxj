8f7f1c3fbe4251ece0bda14a81c0a3c825974e80
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-25 18:49:00 +0000

    * Added dd tag with CSS for CHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3094 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -3685,13 +3685,29 @@ s_chtml10_end_dt_tag(void *pdoc, Node *UNUSED(child))
  * @return The conversion result is returned.
  */
 static char *
-s_chtml10_start_dd_tag(void *pdoc, Node *UNUSED(child))
+s_chtml10_start_dd_tag(void *pdoc, Node *node)
 {
   chtml10_t *chtml10;
   Doc *doc;
+  Attr *attr;
+  char *attr_style = NULL;
+
   chtml10 = GET_CHTML10(pdoc);
   doc     = chtml10->doc;
+
+  for (attr = qs_get_attr(doc,node);
+       attr;
+       attr = qs_get_next_attr(doc,attr)) {
+    char *nm  = qs_get_attr_name(doc,attr);
+    char *val = qs_get_attr_value(doc,attr);
+    if (val && STRCASEEQ('s','S',"style", nm)) {
+      attr_style = val;
+    }
+  }
   W_L("<dd>");
+  if (IS_CSS_ON(chtml10->entryp)) {
+    s_chtml10_push_and_get_now_style(pdoc, node, attr_style);
+  }
   return chtml10->out;
 }
 
@@ -3708,6 +3724,9 @@ static char *
 s_chtml10_end_dd_tag(void *pdoc, Node *UNUSED(child))
 {
   chtml10_t *chtml10 = GET_CHTML10(pdoc);
+  if (IS_CSS_ON(chtml10->entryp)) {
+    chxj_css_pop_prop_list(chtml10->css_prop_stack);
+  }
   return chtml10->out;
 }
 
