fcf12b5245cb70b0e2ddbe74eae4f0cfcf773944
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-26 16:53:27 +0000

    * Added span tag with CSS for SoftBank XHTML converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3128 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_jxhtml.c
+++ src/chxj_jxhtml.c
@@ -118,6 +118,8 @@ static char *s_jxhtml_start_marquee_tag (void *pdoc, Node *node);
 static char *s_jxhtml_end_marquee_tag  (void *pdoc, Node *node);
 static char *s_jxhtml_newline_mark       (void *pdoc, Node *node);
 static char *s_jxhtml_link_tag           (void *pdoc, Node *node);
+static char *s_jxhtml_start_span_tag     (void *pdoc, Node *node);
+static char *s_jxhtml_end_span_tag       (void *pdoc, Node *node);
 
 static void  s_init_jxhtml(jxhtml_t *jxhtml, Doc *doc, request_rec *r, device_table *spec);
 
@@ -318,8 +320,8 @@ tag_handler jxhtml_handler[] = {
   },
   /* tagSPAN */
   {
-    NULL,
-    NULL,
+    s_jxhtml_start_span_tag,
+    s_jxhtml_end_span_tag,
   },
   /* tagTEXT */
   {
@@ -5105,26 +5107,14 @@ s_jxhtml_start_marquee_tag(void *pdoc, Node *node)
       }
       for (cur = size_prop->next; cur != size_prop; cur = cur->next) {
         if (cur->value && *cur->value) {
-          if (STRCASEEQ('x','X',"xx-small",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "1");
-          }
-          else if (STRCASEEQ('x','X',"x-small",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "2");
-          }
-          else if (STRCASEEQ('s','S',"small",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "3");
-          }
-          else if (STRCASEEQ('m','M',"medium",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "4");
-          }
-          else if (STRCASEEQ('l','L',"large",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "5");
-          }
-          else if (STRCASEEQ('x','X',"x-large",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "6");
-          }
-          else if (STRCASEEQ('x','X',"xx-large",cur->value)) {
-            attr_size = apr_pstrdup(doc->pool, "7");
+          if ( STRCASEEQ('x','X',"xx-small",cur->value)
+            || STRCASEEQ('x','X',"x-small", cur->value)
+            || STRCASEEQ('s','S',"small",   cur->value)
+            || STRCASEEQ('m','M',"medium",  cur->value)
+            || STRCASEEQ('l','L',"large",   cur->value)
+            || STRCASEEQ('x','X',"x-large", cur->value)
+            || STRCASEEQ('x','X',"xx-large",cur->value)) {
+            attr_size = apr_pstrdup(doc->pool, cur->value);
           }
         }
       }
@@ -5310,6 +5300,187 @@ s_jxhtml_nopush_and_get_now_style(void *pdoc, Node *node, const char *style_attr
     }
   }
   return last_css;
+}
+
+
+/**
+ * It is a handler who processes the SPAN tag.
+ *
+ * @param pdoc  [i/o] The pointer to the JHTML structure at the output
+ *                     destination is specified.
+ * @param node   [i]   The SPAN tag node is specified.
+ * @return The conversion result is returned.
+ */
+static char *
+s_jxhtml_start_span_tag(void *pdoc, Node *node)
+{
+  jxhtml_t *jxhtml;
+  Doc *doc;
+  Attr *attr;
+  char *attr_style = NULL;
+  char *attr_color = NULL;
+  char *attr_size = NULL;
+  char *attr_align = NULL;
+  char *attr_blink = NULL;
+  char *attr_marquee = NULL;
+  char *attr_marquee_dir = NULL;
+  char *attr_marquee_style = NULL;
+  char *attr_marquee_loop = NULL;
+
+  jxhtml = GET_JXHTML(pdoc);
+  doc     = jxhtml->doc;
+
+  for (attr = qs_get_attr(doc,node);
+       attr;
+       attr = qs_get_next_attr(doc,attr)) {
+    char *nm  = qs_get_attr_name(doc,attr);
+    char *val = qs_get_attr_value(doc,attr);
+    if (val && STRCASEEQ('s','S',"style", nm)) {
+      attr_style = val;
+    }
+  }
+  if (IS_CSS_ON(jxhtml->entryp)) {
+    css_prop_list_t *style = s_jxhtml_push_and_get_now_style(pdoc, node, attr_style);
+    if (style) {
+      css_property_t *color_prop = chxj_css_get_property_value(doc, style, "color");
+      css_property_t *size_prop = chxj_css_get_property_value(doc, style, "font-size");
+      css_property_t *text_align_prop = chxj_css_get_property_value(doc, style, "text-align");
+      css_property_t *decoration_prop = chxj_css_get_property_value(doc, style, "text-decoration");
+      css_property_t *display_prop = chxj_css_get_property_value(doc, style, "display");
+      css_property_t *marquee_dir_prop = chxj_css_get_property_value(doc, style, "-wap-marquee-dir");
+      css_property_t *marquee_style_prop = chxj_css_get_property_value(doc, style, "-wap-marquee-style");
+      css_property_t *marquee_loop_prop = chxj_css_get_property_value(doc, style, "-wap-marquee-loop");
+      css_property_t *cur;
+      for (cur = color_prop->next; cur != color_prop; cur = cur->next) {
+        attr_color = apr_pstrdup(doc->pool, cur->value);
+      }
+      for (cur = size_prop->next; cur != size_prop; cur = cur->next) {
+        if (cur->value && *cur->value) {
+          if ( STRCASEEQ('x','X',"xx-small",cur->value)
+            || STRCASEEQ('x','X',"x-small", cur->value)
+            || STRCASEEQ('s','S',"small",   cur->value)
+            || STRCASEEQ('m','M',"medium",  cur->value)
+            || STRCASEEQ('l','L',"large",   cur->value)
+            || STRCASEEQ('x','X',"x-large", cur->value)
+            || STRCASEEQ('x','X',"xx-large",cur->value)) {
+            attr_size = apr_pstrdup(doc->pool, cur->value);
+          }
+        }
+      }
+      for (cur = decoration_prop->next; cur != decoration_prop; cur = cur->next) {
+        if (cur->value && STRCASEEQ('b','B',"blink",cur->value)) {
+          attr_blink = apr_pstrdup(doc->pool, cur->value);
+        }
+      }
+      for (cur = display_prop->next; cur != display_prop; cur = cur->next) {
+        if (cur->value && strcasecmp("-wap-marquee",cur->value) == 0) {
+          attr_marquee = apr_pstrdup(doc->pool, cur->value);
+        }
+      }
+      for (cur = marquee_dir_prop->next; cur != marquee_dir_prop; cur = cur->next) {
+        if (cur->value && *cur->value) {
+          if ( STRCASEEQ('l','L',"ltr",cur->value)
+            || STRCASEEQ('r','R',"rtl",cur->value)) {
+            attr_marquee_dir = apr_pstrdup(doc->pool, cur->value);
+          }
+        }
+      }
+      for (cur = marquee_style_prop->next; cur != marquee_style_prop; cur = cur->next) {
+        if (cur->value && *cur->value) {
+          if ( STRCASEEQ('s','S',"scroll",cur->value)
+            || STRCASEEQ('s','S',"slide",cur->value)
+            || STRCASEEQ('a','A',"alternate",cur->value)) {
+            attr_marquee_style = apr_pstrdup(doc->pool, cur->value);
+          }
+        }
+      }
+      for (cur = marquee_loop_prop->next; cur != marquee_loop_prop; cur = cur->next) {
+        if (cur->value && *cur->value) {
+          attr_marquee_loop = apr_pstrdup(doc->pool, cur->value);
+        }
+      }
+      for (cur = text_align_prop->next; cur != text_align_prop; cur = cur->next) {
+        if (STRCASEEQ('l','L',"left", cur->value)) {
+          attr_align = apr_pstrdup(doc->pool, "left");
+        }
+        else if (STRCASEEQ('c','C',"center",cur->value)) {
+          attr_align = apr_pstrdup(doc->pool, "center");
+        }
+        else if (STRCASEEQ('r','R',"right",cur->value)) {
+          attr_align = apr_pstrdup(doc->pool, "right");
+        }
+      }
+    }
+  }
+
+  W_L("<span");
+  if (attr_color || attr_size || attr_align || attr_blink || attr_marquee) {
+    W_L(" style=\"");
+    if (attr_color) {
+      attr_color = chxj_css_rgb_func_to_value(doc->pool, attr_color);
+      W_L("color:");
+      W_V(attr_color);
+      W_L(";");
+    }
+    if (attr_size) {
+      W_L("font-size:");
+      W_V(attr_size);
+      W_L(";");
+    }
+    if (attr_align) {
+      W_L("text-align:");
+      W_V(attr_align);
+      W_L(";");
+    }
+    if (attr_blink) {
+      W_L("text-decoration:");
+      W_V("blink");
+      W_L(";");
+    }
+    if (attr_marquee) {
+      W_L("display:-wap-marquee;");
+      if (attr_marquee_dir) {
+        W_L("-wap-marquee-dir:");
+        W_V(attr_marquee_dir);
+        W_L(";");
+      }
+      if (attr_marquee_style) {
+        W_L("-wap-marquee-style:");
+        W_V(attr_marquee_style);
+        W_L(";");
+      }
+      if (attr_marquee_loop) {
+        W_L("-wap-marquee-loop:");
+        W_V(attr_marquee_loop);
+        W_L(";");
+      }
+    }
+    W_L("\"");
+  }
+  W_L(">");
+  return jxhtml->out;
+}
+
+
+/**
+ * It is a handler who processes the SPAN tag.
+ *
+ * @param pdoc  [i/o] The pointer to the JHTML structure at the output
+ *                     destination is specified.
+ * @param node   [i]   The SPAN tag node is specified.
+ * @return The conversion result is returned.
+ */
+static char *
+s_jxhtml_end_span_tag(void *pdoc, Node *UNUSED(node))
+{
+  jxhtml_t *jxhtml = GET_JXHTML(pdoc);
+  Doc *doc = jxhtml->doc;
+
+  W_L("</span>");
+  if (IS_CSS_ON(jxhtml->entryp)) {
+    chxj_css_pop_prop_list(jxhtml->css_prop_stack);
+  }
+  return jxhtml->out;
 }
 /*
  * vim:ts=2 et
