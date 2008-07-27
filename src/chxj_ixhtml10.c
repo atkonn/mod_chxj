3aabcff1243abbf5f992a149fbb6cdb91700c414
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-07-27 13:56:43 +0000

    * Added h6 tag for docomo XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3150 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_ixhtml10.c
+++ src/chxj_ixhtml10.c
@@ -4690,15 +4690,15 @@ s_ixhtml10_start_h6_tag(void *pdoc, Node *node)
       }
     }
   }
-  W_L("<div");
-  W_L(" style=\"");
-  W_L("font-size:xx-small;");
+  W_L("<h6");
   if (attr_align) {
+    W_L(" style=\"");
     W_L("text-align:");
     W_V(attr_align);
     W_L(";");
+    W_L("\"");
   }
-  W_L("\">");
+  W_L(">");
 
   return ixhtml10->out;
 }
@@ -4723,7 +4723,7 @@ s_ixhtml10_end_h6_tag(void *pdoc, Node *UNUSED(child))
   doc     = ixhtml10->doc;
   r       = doc->r;
 
-  W_L("</div>");
+  W_L("</h6>");
   if (IS_CSS_ON(ixhtml10->entryp)) {
     chxj_css_pop_prop_list(ixhtml10->css_prop_stack);
   }
