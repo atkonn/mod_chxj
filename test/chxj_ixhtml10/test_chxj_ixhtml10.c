8c6e2bbcf1fd8df7a8fdb90fe448f330605b8ea8
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-05 17:26:32 +0000

    * Added test code of the chxjif tag for docomo XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3254 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -435,6 +435,7 @@ void test_ixhtml10_div_tag_007();
 void test_ixhtml10_chxjif_tag_001();
 void test_ixhtml10_chxjif_tag_002();
 void test_ixhtml10_chxjif_tag_003();
+void test_ixhtml10_chxjif_tag_004();
 #if 0
 
 
@@ -1439,6 +1440,7 @@ main()
   CU_add_test(ixhtml10_suite, "test <chxj:if>.",                                    test_ixhtml10_chxjif_tag_001);
   CU_add_test(ixhtml10_suite, "test <chxj:if> 2.",                                  test_ixhtml10_chxjif_tag_002);
   CU_add_test(ixhtml10_suite, "test <chxj:if> 3.",                                  test_ixhtml10_chxjif_tag_003);
+  CU_add_test(ixhtml10_suite, "test <chxj:if> 4.",                                  test_ixhtml10_chxjif_tag_004);
 #if 0
   /*=========================================================================*/
   /* <BLOCKQUOTE>                                                            */
@@ -31206,6 +31208,43 @@ void test_ixhtml10_chxjif_tag_002()
 void test_ixhtml10_chxjif_tag_003()
 {
 #define  TEST_STRING   "<chxj:if lang=\"chtml\">" \
+                       "abc" \
+                       "</chxj:if>"
+#define  RESULT_STRING "\n" \
+                       ""
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get_span020;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+  entry.action |= CONVRULE_CSS_ON_BIT;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_ixhtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", TEST_RESULT);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 0);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_ixhtml10_chxjif_tag_004()
+{
+#define  TEST_STRING   "<chxj:if lang=\"hdml\">" \
                        "abc" \
                        "</chxj:if>"
 #define  RESULT_STRING "\n" \
