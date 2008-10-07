a9c611b7fd848870a844d93ceae26ad79ef03ad2
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-07 15:20:45 +0000

    * Added test code of the small tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3264 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -445,6 +445,7 @@ void test_ixhtml10_nobr_tag_001();
 void test_ixhtml10_nobr_tag_002();
 
 void test_ixhtml10_small_tag_001();
+void test_ixhtml10_small_tag_002();
 #if 0
 
 
@@ -1465,6 +1466,7 @@ main()
   /* <SMALL>                                                                 */
   /*=========================================================================*/
   CU_add_test(ixhtml10_suite, "test <small> 1.",                                    test_ixhtml10_small_tag_001);
+  CU_add_test(ixhtml10_suite, "test <small> 2.",                                    test_ixhtml10_small_tag_002);
 
 #if 0
   /*=========================================================================*/
@@ -31529,6 +31531,40 @@ void test_ixhtml10_small_tag_001()
 {
 #define  TEST_STRING   "<small></small>"
 #define  RESULT_STRING "<small></small>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get_span020;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+  entry.action |= CONVRULE_CSS_ON_BIT;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_ixhtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 0);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_ixhtml10_small_tag_002()
+{
+#define  TEST_STRING   "<small>abc</small>"
+#define  RESULT_STRING "<small>abc</small>"
   char  *ret;
   char  *tmp;
   device_table spec;
