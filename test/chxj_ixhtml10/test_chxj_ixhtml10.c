dbf69d2465f153543d4f2c8f4e63f20958cf7fef
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-07 14:50:27 +0000

    * Added test code of the nobr tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3261 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -442,6 +442,7 @@ void test_ixhtml10_chxjif_tag_007();
 void test_ixhtml10_chxjif_tag_008();
 
 void test_ixhtml10_nobr_tag_001();
+void test_ixhtml10_nobr_tag_002();
 #if 0
 
 
@@ -1456,6 +1457,7 @@ main()
   /* <NOBR>                                                                  */
   /*=========================================================================*/
   CU_add_test(ixhtml10_suite, "test <nobr> 1.",                                     test_ixhtml10_nobr_tag_001);
+  CU_add_test(ixhtml10_suite, "test <nobr> 2.",                                     test_ixhtml10_nobr_tag_002);
 
 #if 0
   /*=========================================================================*/
@@ -31449,6 +31451,40 @@ void test_ixhtml10_nobr_tag_001()
 {
 #define  TEST_STRING   "<nobr></nobr>"
 #define  RESULT_STRING "<nobr></nobr>"
+  char  *ret;
+  char  *tmp;
+  device_table spec;
+  chxjconvrule_entry entry;
+  cookie_t cookie;
+  apr_size_t destlen;
+  APR_INIT;
+  chxj_serf_get = test_chxj_serf_get_span020;
+  call_check = 0;
+
+  COOKIE_INIT(cookie);
+
+  SPEC_INIT(spec);
+  destlen = sizeof(TEST_STRING)-1;
+  entry.action |= CONVRULE_CSS_ON_BIT;
+
+  tmp = chxj_encoding(&r, TEST_STRING, &destlen);
+  ret = chxj_convert_ixhtml10(&r, &spec, tmp, destlen, &destlen, &entry, &cookie);
+  ret = chxj_rencoding(&r, ret, &destlen);
+  fprintf(stderr, "actual:[%s]\n", ret);
+  fprintf(stderr, "expect:[%s]\n", RESULT_STRING);
+  CU_ASSERT(ret != NULL);
+  CU_ASSERT(strcmp(RESULT_STRING, ret) == 0);
+  CU_ASSERT(destlen == sizeof(RESULT_STRING)-1);
+  CU_ASSERT(call_check == 0);
+
+  APR_TERM;
+#undef TEST_STRING
+#undef RESULT_STRING
+}
+void test_ixhtml10_nobr_tag_002()
+{
+#define  TEST_STRING   "<nobr>abc</nobr>"
+#define  RESULT_STRING "<nobr>abc</nobr>"
   char  *ret;
   char  *tmp;
   device_table spec;
