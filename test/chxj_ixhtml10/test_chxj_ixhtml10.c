b319315c9ef3d51c75bebaa5e4f7799d44aade01
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-12 19:11:42 +0000

    * Added test code of the dt tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3315 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -23746,9 +23746,10 @@ void test_ixhtml10_dt_tag_with_css_003()
 {
 #define  TEST_STRING "<html><head><link rel=\"stylesheet\" href=\"http://localhost/a.css\"  type=\"text/css\" />" \
                      "</head><body><dl><dt>あいう</dt></dl></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?>" \
-                       "<!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
-                       "<html><head></head><body><div><dl><dt style=\"font-size:x-small;\">あいう</dt></dl></div></body></html>"
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
+                       "<head></head><body><dl><dt style=\"font-size:x-small;\">あいう</dt></dl></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
