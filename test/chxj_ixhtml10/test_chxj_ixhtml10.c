be73f72bdf3a2221e783767f33bcd4d0f0d39976
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-17 17:32:11 +0000

    * Added test code of the dd tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3412 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -26763,7 +26763,7 @@ void test_ixhtml10_dd_tag_with_css_009()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><div><dd style=\"font-size:xx-small;\">あいう</dd></div></body></html>"
+                       "<head></head><body><dd style=\"font-size:xx-small;\">あいう</dd></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
