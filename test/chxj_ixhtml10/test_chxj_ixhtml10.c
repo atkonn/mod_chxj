eb811c22a6c71f6118223717208038b49f567183
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-17 16:18:34 +0000

    * Added test code of the dir tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3353 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -552,6 +552,29 @@ void test_ixhtml10_dir_tag_004();
 void test_ixhtml10_dir_tag_005();
 void test_ixhtml10_dir_tag_006();
 void test_ixhtml10_dir_tag_007();
+
+void test_ixhtml10_dir_tag_with_css_001();
+void test_ixhtml10_dir_tag_with_css_002();
+void test_ixhtml10_dir_tag_with_css_003();
+void test_ixhtml10_dir_tag_with_css_004();
+void test_ixhtml10_dir_tag_with_css_005();
+void test_ixhtml10_dir_tag_with_css_006();
+void test_ixhtml10_dir_tag_with_css_007();
+void test_ixhtml10_dir_tag_with_css_008();
+void test_ixhtml10_dir_tag_with_css_009();
+void test_ixhtml10_dir_tag_with_css_010();
+void test_ixhtml10_dir_tag_with_css_011();
+void test_ixhtml10_dir_tag_with_css_012();
+void test_ixhtml10_dir_tag_with_css_013();
+void test_ixhtml10_dir_tag_with_css_014();
+void test_ixhtml10_dir_tag_with_css_015();
+void test_ixhtml10_dir_tag_with_css_016();
+void test_ixhtml10_dir_tag_with_css_017();
+void test_ixhtml10_dir_tag_with_css_018();
+void test_ixhtml10_dir_tag_with_css_019();
+void test_ixhtml10_dir_tag_with_css_020();
+void test_ixhtml10_dir_tag_with_css_021();
+void test_ixhtml10_dir_tag_with_css_022();
 #if 0
 
 
@@ -873,28 +896,6 @@ void test_ixhtml10_div_tag_with_css_042();
 
 
 
-void test_ixhtml10_dir_tag_with_css_001();
-void test_ixhtml10_dir_tag_with_css_002();
-void test_ixhtml10_dir_tag_with_css_003();
-void test_ixhtml10_dir_tag_with_css_004();
-void test_ixhtml10_dir_tag_with_css_005();
-void test_ixhtml10_dir_tag_with_css_006();
-void test_ixhtml10_dir_tag_with_css_007();
-void test_ixhtml10_dir_tag_with_css_008();
-void test_ixhtml10_dir_tag_with_css_009();
-void test_ixhtml10_dir_tag_with_css_010();
-void test_ixhtml10_dir_tag_with_css_011();
-void test_ixhtml10_dir_tag_with_css_012();
-void test_ixhtml10_dir_tag_with_css_013();
-void test_ixhtml10_dir_tag_with_css_014();
-void test_ixhtml10_dir_tag_with_css_015();
-void test_ixhtml10_dir_tag_with_css_016();
-void test_ixhtml10_dir_tag_with_css_017();
-void test_ixhtml10_dir_tag_with_css_018();
-void test_ixhtml10_dir_tag_with_css_019();
-void test_ixhtml10_dir_tag_with_css_020();
-void test_ixhtml10_dir_tag_with_css_021();
-void test_ixhtml10_dir_tag_with_css_022();
 
 void test_ixhtml10_dl_tag_with_css_001();
 void test_ixhtml10_dl_tag_with_css_002();
