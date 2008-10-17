108d53968373ee3bb67bcc580c485236dd2107dd
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-17 16:19:26 +0000

    * Added test code of the dir tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3370 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -25315,10 +25315,10 @@ void test_ixhtml10_dir_tag_with_css_008()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body><dir style=\"list-style-type:square\">あいう</dir></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?>" \
-                       "<!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
-                       "<html>" \
-                       "<head></head><body><div><dir style=\"list-style-type:square;\">あいう</dir></div></body></html>"
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
+                       "<head></head><body><ul style=\"list-style-type:square;\">あいう</ul></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
