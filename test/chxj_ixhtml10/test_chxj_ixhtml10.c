5feeb45cf860ea6b3000544fd69767f3723f71a3
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-17 17:46:58 +0000

    * Added test code of the menu tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3438 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -27392,10 +27392,10 @@ void test_ixhtml10_menu_tag_with_css_002()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body><menu style=\"color:#ff0000\">あいう</menu></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?>" \
-                       "<!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
-                       "<html>" \
-                       "<head></head><body><div><menu style=\"color:#ff0000;\">あいう</menu></div></body></html>"
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
+                       "<head></head><body><ul style=\"color:#ff0000;\">あいう</ul></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
