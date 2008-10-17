a20368aa141611c3f7b5fa81dc9e9b163a7b3ef2
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-17 17:20:18 +0000

    * Added test code of the dl tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3402 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -26532,10 +26532,10 @@ void test_ixhtml10_dl_tag_with_css_020()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body><dl style=\"font-size:x-large\">あいう</dl></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?>" \
-                       "<!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
-                       "<html>" \
-                       "<head></head><body><div><dl style=\"font-size:x-large;\">あいう</dl></div></body></html>"
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
+                       "<head></head><body><dl style=\"font-size:x-large;\">あいう</dl></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
