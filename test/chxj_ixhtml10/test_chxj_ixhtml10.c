d8d6471eb38717bda6e57b6d312acefb00454fa0
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-19 15:55:21 +0000

    * Added test code of the blink tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3481 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -28789,10 +28789,10 @@ void test_ixhtml10_blink_tag_with_css_022()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body><blink style=\"font-size:xx-large\">あいう</blink></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?>" \
-                       "<!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
-                       "<html>" \
-                       "<head></head><body><div><blink style=\"font-size:xx-large;\">あいう</blink></div></body></html>"
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
+                       "<head></head><body><span style=\"text-decoration:blink;font-size:xx-large;\">あいう</span></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
