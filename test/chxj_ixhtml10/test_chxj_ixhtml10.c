52be169aa67b6b74434bd07f79a99993c890f1e4
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-21 14:32:41 +0000

    * Added test code of the marquee tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3486 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -1744,6 +1744,23 @@ main()
   CU_add_test(ixhtml10_suite, "test <marquee> 19." ,                               test_ixhtml10_marquee_tag_019);
   CU_add_test(ixhtml10_suite, "test <marquee> 20." ,                               test_ixhtml10_marquee_tag_020);
   CU_add_test(ixhtml10_suite, "test <marquee> 21." ,                               test_ixhtml10_marquee_tag_021);
+
+  CU_add_test(ixhtml10_suite, "test marquee with css 001",                       test_ixhtml10_marquee_tag_with_css_001);
+  CU_add_test(ixhtml10_suite, "test marquee with css 002",                       test_ixhtml10_marquee_tag_with_css_002);
+  CU_add_test(ixhtml10_suite, "test marquee with css 009",                       test_ixhtml10_marquee_tag_with_css_009);
+  CU_add_test(ixhtml10_suite, "test marquee with css 010",                       test_ixhtml10_marquee_tag_with_css_010);
+  CU_add_test(ixhtml10_suite, "test marquee with css 011",                       test_ixhtml10_marquee_tag_with_css_011);
+  CU_add_test(ixhtml10_suite, "test marquee with css 012",                       test_ixhtml10_marquee_tag_with_css_012);
+  CU_add_test(ixhtml10_suite, "test marquee with css 013",                       test_ixhtml10_marquee_tag_with_css_013);
+  CU_add_test(ixhtml10_suite, "test marquee with css 014",                       test_ixhtml10_marquee_tag_with_css_014);
+  CU_add_test(ixhtml10_suite, "test marquee with css 015",                       test_ixhtml10_marquee_tag_with_css_015);
+  CU_add_test(ixhtml10_suite, "test marquee with css 016",                       test_ixhtml10_marquee_tag_with_css_016);
+  CU_add_test(ixhtml10_suite, "test marquee with css 017",                       test_ixhtml10_marquee_tag_with_css_017);
+  CU_add_test(ixhtml10_suite, "test marquee with css 018",                       test_ixhtml10_marquee_tag_with_css_018);
+  CU_add_test(ixhtml10_suite, "test marquee with css 019",                       test_ixhtml10_marquee_tag_with_css_019);
+  CU_add_test(ixhtml10_suite, "test marquee with css 020",                       test_ixhtml10_marquee_tag_with_css_020);
+  CU_add_test(ixhtml10_suite, "test marquee with css 021",                       test_ixhtml10_marquee_tag_with_css_021);
+  CU_add_test(ixhtml10_suite, "test marquee with css 022",                       test_ixhtml10_marquee_tag_with_css_022);
 #if 0
   /*=========================================================================*/
   /* <FORM>                                                                  */
@@ -2001,22 +2018,6 @@ main()
 
 
 
-  CU_add_test(ixhtml10_suite, "test marquee with css 001",                       test_ixhtml10_marquee_tag_with_css_001);
-  CU_add_test(ixhtml10_suite, "test marquee with css 002",                       test_ixhtml10_marquee_tag_with_css_002);
-  CU_add_test(ixhtml10_suite, "test marquee with css 009",                       test_ixhtml10_marquee_tag_with_css_009);
-  CU_add_test(ixhtml10_suite, "test marquee with css 010",                       test_ixhtml10_marquee_tag_with_css_010);
-  CU_add_test(ixhtml10_suite, "test marquee with css 011",                       test_ixhtml10_marquee_tag_with_css_011);
-  CU_add_test(ixhtml10_suite, "test marquee with css 012",                       test_ixhtml10_marquee_tag_with_css_012);
-  CU_add_test(ixhtml10_suite, "test marquee with css 013",                       test_ixhtml10_marquee_tag_with_css_013);
-  CU_add_test(ixhtml10_suite, "test marquee with css 014",                       test_ixhtml10_marquee_tag_with_css_014);
-  CU_add_test(ixhtml10_suite, "test marquee with css 015",                       test_ixhtml10_marquee_tag_with_css_015);
-  CU_add_test(ixhtml10_suite, "test marquee with css 016",                       test_ixhtml10_marquee_tag_with_css_016);
-  CU_add_test(ixhtml10_suite, "test marquee with css 017",                       test_ixhtml10_marquee_tag_with_css_017);
-  CU_add_test(ixhtml10_suite, "test marquee with css 018",                       test_ixhtml10_marquee_tag_with_css_018);
-  CU_add_test(ixhtml10_suite, "test marquee with css 019",                       test_ixhtml10_marquee_tag_with_css_019);
-  CU_add_test(ixhtml10_suite, "test marquee with css 020",                       test_ixhtml10_marquee_tag_with_css_020);
-  CU_add_test(ixhtml10_suite, "test marquee with css 021",                       test_ixhtml10_marquee_tag_with_css_021);
-  CU_add_test(ixhtml10_suite, "test marquee with css 022",                       test_ixhtml10_marquee_tag_with_css_022);
 
 
 #endif
