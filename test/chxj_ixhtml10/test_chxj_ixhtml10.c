7d17bbef3a0c6401b37b5b165614ad96ff7a2b16
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-10-21 14:33:38 +0000

    * Added test code of the marquee tag for XHTML1.0 converter.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/sandbox@3504 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -13651,7 +13651,7 @@ void test_ixhtml10_marquee_tag_019()
 void test_ixhtml10_marquee_tag_020()
 {
 #define  TEST_STRING "<marquee bgcolor=\"\">ﾊﾝｶｸ</marquee>"
-#define  RESULT_STRING "<marquee>ﾊﾝｶｸ</marquee>"
+#define  RESULT_STRING "<span style=\"display:-wap-marquee;\">ﾊﾝｶｸ</span>"
   char  *ret;
   char  *tmp;
   device_table spec;
