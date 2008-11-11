7b53544980536beb22490dd0ea9d11b068b9890e
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-11 18:27:14 +0000

    * Added test code of CSS for h2 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3787 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -17988,7 +17988,9 @@ void test_ixhtml10_h2_tag_with_css_004()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body><h2 style=\"text-align: left\">あいう</h2></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?><!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
                        "<html><head></head><body><div><div style=\"font-size:x-large;text-align:left;\">あいう</div></div></body></html>"
   char  *ret;
   char  *tmp;
