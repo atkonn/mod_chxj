3c401afae578f75f4104b5c3c65a97de4e85ba85
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-11 17:38:15 +0000

    * Added test code of CSS for h1 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3748 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -17721,7 +17721,7 @@ void test_ixhtml10_h1_tag_with_css_004()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><div style=\"font-size:xx-large;text-align:left;\">あいう</div></body></html>"
+                       "<head></head><body><h1 style=\"font-size:xx-large;text-align:left;\">あいう</h1></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
