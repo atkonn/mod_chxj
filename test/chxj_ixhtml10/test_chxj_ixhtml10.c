619e0f2fdef8339ea7b838a22a46111ea471b51e
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:06:51 +0000

    * Added test code of CSS for h3 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3837 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -18297,7 +18297,7 @@ void test_ixhtml10_h3_tag_with_css_005()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><div style=\"font-size:large;text-align:center;\">あいう</div></div></body></html>"
+                       "<head></head><body><div style=\"font-size:large;text-align:center;\">あいう</div></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
