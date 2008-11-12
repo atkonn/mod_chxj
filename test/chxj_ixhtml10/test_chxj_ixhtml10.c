caf2ae089293606b025352de869b61d6eb97d352
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:15:22 +0000

    * Added test code of CSS for body tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3980 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -19289,8 +19289,9 @@ void test_ixhtml10_body_tag_with_css_003()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body style=\"color:#ffff00\">あいう</body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?>" \
-                       "<!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
                        "<html><head></head><body style=\"color:#ffff00;\"><div>あいう</div></body></html>"
   char  *ret;
   char  *tmp;
