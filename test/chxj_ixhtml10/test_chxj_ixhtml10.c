142e306933b6d05034da83979fa5fae20e487369
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:11:02 +0000

    * Added test code of CSS for h5 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3913 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -18798,7 +18798,7 @@ void test_ixhtml10_h5_tag_with_css_004()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><div style=\"font-size:x-small;text-align:left;\">あいう</div></body></html>"
+                       "<head></head><body><h5 style=\"font-size:x-small;text-align:left;\">あいう</h5></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
