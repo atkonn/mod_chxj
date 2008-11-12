419eeac27fe1fec0334d6388bc2e438a99b82b48
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:07:36 +0000

    * Added test code of CSS for h4 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3851 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -18391,7 +18391,7 @@ void test_ixhtml10_h4_tag_with_css_001()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><h4 style=\"font-size:small;text-align:right;\">あいう</h4></body></html>"
+                       "<head></head><body><h4 style=\"text-align:right;\">あいう</h4></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
