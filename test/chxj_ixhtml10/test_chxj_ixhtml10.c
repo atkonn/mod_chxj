4a38eb7f4a2d6b22ca249f2911acdf0687edb29b
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:16:44 +0000

    * Added test code of CSS for font tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3999 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -19471,7 +19471,7 @@ void test_ixhtml10_font_tag_with_css_003()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<html><head></head><span style=\"font-size: xx-small\">あいう</span></html>"
+                       "<head></head><span style=\"font-size: xx-small\">あいう</span></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
