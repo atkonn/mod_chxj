d29d87305b885e56db5f3a951fcd1693d64b88f6
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:13:49 +0000

    * Added test code of CSS for h6 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3958 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -19104,7 +19104,9 @@ void test_ixhtml10_h6_tag_with_css_005()
 {
 #define  TEST_STRING "<html><head>" \
                      "</head><body><h6 style=\"text-align: center\">あいう</h6></body></html>"
-#define  RESULT_STRING "<?xml version='1.0' encoding='Shift_JIS' ?><!DOCTYPE html PUBLIC \"-//J-PHONE//DTD XHTML Basic 1.0 Plus//EN\" \"html-basic10-plus.dtd\">" \
+#define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
+                       "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
+                       "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
                        "<html><head></head><body><div><div style=\"font-size:xx-small;text-align:center;\">あいう</div></div></body></html>"
   char  *ret;
   char  *tmp;
