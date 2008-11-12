b238b017a912aa08c1b3ddc86a246f1afc6c820c
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:22:15 +0000

    * Added test code of CSS for form tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@4070 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -20353,7 +20353,7 @@ void test_ixhtml10_form_tag_with_css_007()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<html><head></head><form><div align=\"right\">あいう</div></form></html>"
+                       "<head></head><form><div align=\"right\">あいう</div></form></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
