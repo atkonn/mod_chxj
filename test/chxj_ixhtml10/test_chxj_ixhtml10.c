0ba4d1262e69396f3a06234459db9e1c1440b5d0
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-12 17:14:41 +0000

    * Added test code of CSS for h6 tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@3970 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -19145,7 +19145,7 @@ void test_ixhtml10_h6_tag_with_css_006()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><h6 style=\"font-size:xx-small;text-align:right;\">あいう</h6></body></html>"
+                       "<head></head><body><h6 style=\"text-align:right;\">あいう</h6></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
