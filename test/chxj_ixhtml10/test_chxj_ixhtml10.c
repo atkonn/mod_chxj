7b7fa470ae08ea4915efdfad40db89a011724eb1
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-13 17:14:45 +0000

    * Added test code of CSS for div tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@4464 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -24009,7 +24009,7 @@ void test_ixhtml10_div_tag_with_css_039()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<html><head></head><body><div><div style=\"font-size:x-large;\">あいう</div></div></body></html>"
+                       "<head></head><body><div><div style=\"font-size:x-large;\">あいう</div></div></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
