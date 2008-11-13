14dcc8c76bc22094d6c276011cda8ad559ba8727
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-13 16:58:09 +0000

    * Added test code of CSS for hr tag.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_13_0@4217 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- test/chxj_ixhtml10/test_chxj_ixhtml10.c
+++ test/chxj_ixhtml10/test_chxj_ixhtml10.c
@@ -21814,7 +21814,7 @@ void test_ixhtml10_hr_tag_with_css_008()
 #define  RESULT_STRING "<?xml version=\"1.0\" encoding=\"Shift_JIS\" ?>" \
                        "<!DOCTYPE html PUBLIC \"-//i-mode group (ja)//DTD XHTML i-XHTML(Locale/Ver.=ja/1.0) 1.0//EN\" \"i-xhtml_4ja_10.dtd\">" \
                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" \
-                       "<head></head><body><div><hr style=\"width:10%;\" />あいう</div></body></html>"
+                       "<head></head><body><hr style=\"width:10%;\" />あいう</div></body></html>"
   char  *ret;
   char  *tmp;
   device_table spec;
