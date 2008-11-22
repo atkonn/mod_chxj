792440bfad3565d627f990816fb3dac96d3d0376
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-22 18:35:24 +0000

    * Defined  header.
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@4499 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- include/chxj_add_device_env.h
+++ include/chxj_add_device_env.h
@@ -20,6 +20,51 @@
 #include "mod_chxj.h"
 #include "chxj_specified_device.h"
 
+/* "X-Chxj-Provider" => { 0 => 'unknown', 1 => 'docomo', 2 => 'au', 3 => 'SoftBank' */
+#define HTTP_X_CHXJ_PROVIDER "X-Chxj-Provider"
+
+/* "X-Chxj-DeviceId" => { docomo => 'D503i' and so on, au => 'CA31' and so on, SoftBank => '706P' and so on */
+#define HTTP_X_CHXJ_DEVICEID "X-Chxj-DeviceId"
+
+/* "X-Chxj-DeviceName" => { 'SoftBank 706P' and so on. } <device_name> */
+#define HTTP_X_CHXJ_DEVICENAME "X-Chxj-DeviceName"
+
+/* "X-Chxj-HtmlSpecType" => { JXHTML, JHTML, CHTML1.0 CHTML2.0 CHTML3.0 CHTML4.0 CHTML5.0 IXHTML1.0 } */
+#define HTTP_X_CHXJ_HTMLSPECTYPE "X-Chxj-HtmlSpecType"
+
+/* "X-Chxj-Width"        => { <width> } */
+#define HTTP_X_CHXJ_WIDTH "X-Chxj-Width"
+
+/* "X-Chxj-Height"       => { <height> } */
+#define HTTP_X_CHXJ_HEIGHT "X-Chxj-Height"
+
+/* "X-Chxj-Gif"          => { true/false } */
+#define HTTP_X_CHXJ_GIF    "X-Chxj-Gif"
+
+/* "X-Chxj-Jpeg"         => { true/false } */
+#define HTTP_X_CHXJ_JPEG   "X-Chxj-Jpeg"
+
+/* "X-Chxj-Png"          => { true/false } */
+#define HTTP_X_CHXJ_PNG    "X-Chxj-Png"
+
+/* "X-Chxj-Bmp2"         => { true/false } */
+#define HTTP_X_CHXJ_BMP2   "X-Chxj-Bmp2"
+
+/* "X-Chxj-Bmp4"         => { true/false } */
+#define HTTP_X_CHXJ_BMP4   "X-Chxj-Bmp4"
+
+/* "X-Chxj-Color"        => { <color> } */
+#define HTTP_X_CHXJ_COLOR  "X-Chxj-Color"
+
+/* "X-Chxj-WP-Width"     => { <wp_width> } */
+#define HTTP_X_CHXJ_WP_WIDTH "X-Chxj-WP-Width"
+
+/* "X-Chxj-WP-Height"    => { <wp_height> } */
+#define HTTP_X_CHXJ_WP_HEIGHT "X-Chxj-WP-Width"
+
+/* "X-Chxj-Cache"        => { <cache> } */
+#define HTTP_X_CHXJ_CACHE    "X-Chxj-Cache"
+
 extern void chxj_add_device_env(request_rec *r, device_table *spec);
 
 #endif
--- src/chxj_add_device_env.c
+++ src/chxj_add_device_env.c
@@ -21,5 +21,21 @@ void
 chxj_add_device_env(request_rec *r, device_table *spec)
 {
   DBG(r, "REQ[%X] start chxj_add_device_env()", (unsigned int)(apr_size_t)r);
+  /* "X-Chxj-Provider" => { 0 => 'unknown', 1 => 'docomo', 2 => 'au', 3 => 'SoftBank' */
+  /* "X-Chxj-DeviceId" => { docomo => 'D503i' and so on, au => 'CA31' and so on, SoftBank => '706P' and so on */
+  /* "X-Chxj-DeviceName" => { 'SoftBank 706P' and so on. } <device_name> */
+  /* "X-Chxj-HtmlSpecType" => { JXHTML, JHTML, CHTML1.0 CHTML2.0 CHTML3.0 CHTML4.0 CHTML5.0 IXHTML1.0 } */
+  /* "X-Chxj-Width"        => { <width> } */
+  /* "X-Chxj-Height"       => { <height> } */
+  /* "X-Chxj-Gif"          => { true/false } */
+  /* "X-Chxj-Jpeg"         => { true/false } */
+  /* "X-Chxj-Png"          => { true/false } */
+  /* "X-Chxj-Bmp2"         => { true/false } */
+  /* "X-Chxj-Bmp4"         => { true/false } */
+  /* "X-Chxj-Color"        => { <color> } */
+  /* "X-Chxj-WP-Width"     => { <wp_width> } */
+  /* "X-Chxj-WP-Height"    => { <wp_height> } */
+  /* "X-Chxj-Cache"        => { <cache> } */
+
   DBG(r, "REQ[%X] end chxj_add_device_env()", (unsigned int)(apr_size_t)r);
 }
