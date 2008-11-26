a10a7be6927aa82d70df0404f01b9e09a976cfe9
konn <konn@1a406e8e-add9-4483-a2c8-d8cac5b7c224>
2008-11-26 06:16:21 +0000

    * Fixed Bug.     - #14129
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/modchxj/mod_chxj/branches/RELEASE_0_12_0@4535 1a406e8e-add9-4483-a2c8-d8cac5b7c224

--- src/chxj_chtml10.c
+++ src/chxj_chtml10.c
@@ -1501,7 +1501,9 @@ s_chtml10_start_a_tag(void *pdoc, Node *node)
         /* CHTML1.0                                                           */
         /*--------------------------------------------------------------------*/
         value = chxj_encoding_parameter(r, value);
-        value = chxj_add_cookie_parameter(r, value, chtml10->cookie);
+        if (! chxj_starts_with(value, "mailto:") && ! chxj_starts_with(value, "telto:")) {
+          value = chxj_add_cookie_parameter(r, value, chtml10->cookie);
+        }
         W_L(" href=\"");
         W_V(value);
         W_L("\"");
--- src/chxj_jhtml.c
+++ src/chxj_jhtml.c
@@ -1054,7 +1054,9 @@ s_jhtml_start_a_tag(void *pdoc, Node *node)
       /* CHTML1.0                                                             */
       /*----------------------------------------------------------------------*/
       value = chxj_encoding_parameter(r, value);
-      value = chxj_add_cookie_parameter(r, value, jhtml->cookie);
+      if (! chxj_starts_with(value, "mailto:") && ! chxj_starts_with(value, "telto:")) {
+        value = chxj_add_cookie_parameter(r, value, jhtml->cookie);
+      }
       W_L(" href=\"");
       W_V(value);
       W_L("\"");
