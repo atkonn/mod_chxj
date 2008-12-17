451083cfaed9dcb3795fbe530ad91a37d2afa196
Atsushi Konno <konn@users.sourceforge.jp>
2008-12-17 13:23:23 +0900

    * Added QS_CONV definition.

--- include/mod_chxj.h
+++ include/mod_chxj.h
@@ -375,6 +375,8 @@ struct mod_chxj_config {
 #define CONVRULE_Z2H_ALPHA_OFF_BIT    (0x00000040)
 #define CONVRULE_Z2H_NUM_ON_BIT       (0x00000080)
 #define CONVRULE_Z2H_NUM_OFF_BIT      (0x00000100)
+#define CONVRULE_QS_CONV_ON           (0x00000200)
+#define CONVRULE_QS_CONV_OFF          (0x00000400)
 
 #define CONVRULE_ENGINE_ON_CMD        "EngineOn"
 #define CONVRULE_ENGINE_OFF_CMD       "EngineOff"
@@ -388,6 +390,10 @@ struct mod_chxj_config {
 #define CONVRULE_Z2H_NUM_OFF_CMD      "Z2hNumOff"
 #define CONVRULE_Z2H_ALL_ON_CMD       "Z2hAllOn"
 #define CONVRULE_Z2H_ALL_OFF_CMD      "Z2hAllOff"
+#define CONVRULE_Z2H_ALL_ON_CMD       "Z2hAllOn"
+#define CONVRULE_Z2H_ALL_OFF_CMD      "Z2hAllOff"
+#define CONVRULE_QS_CONV_ON_CMD       "QSConvOn"
+#define CONVRULE_QS_CONV_OFF_CMD      "QSConvOff"
 
 
 #define CONVRULE_FLAG_NOTMATCH        (0x00000001)
