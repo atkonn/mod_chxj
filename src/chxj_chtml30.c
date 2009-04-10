cda5a9a0aacab755a3a0415edf95a79eddeec009
Atsushi Konno <konn@users.sourceforge.jp>
2009-04-10 21:13:47 +0900

    * Changed Feature.     - Changed the target of Z2hOn option for CHTML3.0

--- src/chxj_chtml30.c
+++ src/chxj_chtml30.c
@@ -1530,6 +1530,9 @@ s_chtml30_start_input_tag(void *pdoc, Node *node)
     W_L("\"");
   }
   if (value && *value) {
+    apr_size_t value_len = strlen(value);
+    value = chxj_conv_z2h(r, value, &value_len, chtml30->entryp);
+
     W_L(" value=\"");
     W_V(chxj_add_slash_to_doublequote(doc->pool, value));
     W_L("\"");
@@ -2855,6 +2858,7 @@ s_chtml30_text_tag(void *pdoc, Node *child)
   char        one_byte[2];
   int         ii;
   int         tdst_len;
+  apr_size_t  z2h_input_len;
 
   chtml30 = GET_CHTML30(pdoc);
   doc     = chtml30->doc;
@@ -2901,6 +2905,9 @@ s_chtml30_text_tag(void *pdoc, Node *child)
       tdst = qs_out_apr_pstrcat(r, tdst, one_byte, &tdst_len);
     }
   }
+  z2h_input_len = strlen(tdst);
+  tdst = chxj_conv_z2h(r, tdst, &z2h_input_len, chtml30->entryp);
+
   W_V(tdst);
   return chtml30->out;
 }
