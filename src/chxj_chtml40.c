f9432600a664f5bf114a4c2d6d9b8f0eca6eb1e4
Atsushi Konno <konn@users.sourceforge.jp>
2009-04-10 21:45:14 +0900

    * Changed Feature.     - Changed the target of Z2hOn option for CHTML4.0

--- src/chxj_chtml40.c
+++ src/chxj_chtml40.c
@@ -23,6 +23,7 @@
 #include "chxj_qr_code.h"
 #include "chxj_encoding.h"
 #include "chxj_header_inf.h"
+#include "chxj_conv_z2h.h"
 
 #define GET_CHTML40(X) ((chtml40_t *)(X))
 #undef W_L
@@ -1536,6 +1537,11 @@ s_chtml40_start_input_tag(void *pdoc, Node *node)
     W_L("\"");
   }
   if (value && *value) {
+    if (STRCASEEQ('s','S',"submit",type) || STRCASEEQ('r','R',"reset",type)) {
+      apr_size_t value_len = strlen(value);
+      value = chxj_conv_z2h(r, value, &value_len, chtml40->entryp);
+    }
+
     W_L(" value=\"");
     W_V(chxj_add_slash_to_doublequote(doc->pool, value));
     W_L("\"");
@@ -2869,6 +2875,7 @@ s_chtml40_text_tag(void *pdoc, Node *child)
   char        one_byte[2];
   int         ii;
   int         tdst_len;
+  apr_size_t  z2h_input_len;
 
   chtml40 = GET_CHTML40(pdoc);
   doc     = chtml40->doc;
@@ -2915,6 +2922,8 @@ s_chtml40_text_tag(void *pdoc, Node *child)
       tdst = qs_out_apr_pstrcat(r, tdst, one_byte, &tdst_len);
     }
   }
+  z2h_input_len = strlen(tdst);
+  tdst = chxj_conv_z2h(r, tdst, &z2h_input_len, chtml40->entryp);
   W_V(tdst);
   return chtml40->out;
 }
