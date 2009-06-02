8f7160e9b49ad5ca12b3864efc300da7d7377c24
Atsushi Konno <konn@users.sourceforge.jp>
2009-06-02 10:03:00 +0900

    * Fixed Bug.     - http://sourceforge.jp/forum/message.php?msg_id=44122

--- src/chxj_encoding.c
+++ src/chxj_encoding.c
@@ -401,6 +401,7 @@ chxj_encoding_parameter(request_rec *r, const char *value)
       len = (apr_size_t)strlen(key);
       if (klen != len) {
         key = chxj_encoding(r, key, &len);
+        key = chxj_url_encode(r->pool, key);
       }
 #if 0  /* XXX:2009/4/10 */
       key = chxj_url_encode(r->pool, key);
@@ -416,6 +417,7 @@ chxj_encoding_parameter(request_rec *r, const char *value)
       len = (apr_size_t)strlen(val);
       if (vlen != len) {
         val = chxj_encoding(r, val, &len);
+        val = chxj_url_encode(r->pool, val);
       }
 #if 0  /* XXX:2009/4/10 */
       val = chxj_url_encode(r->pool, val);
